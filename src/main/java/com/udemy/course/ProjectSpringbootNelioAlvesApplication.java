19ece9315f98dabc68d90a84743ca107d7b020bc
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-05-12 09:55:12 -0300

    Project created

---
+++ src/main/java/com/udemy/course/ProjectSpringbootNelioAlvesApplication.java
@@ -0,0 +1,20 @@
+package com.udemy.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootNelioAlvesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringbootNelioAlvesApplication.class, args);
+
+
+
+        
+
+
+
+    }
+
+}
