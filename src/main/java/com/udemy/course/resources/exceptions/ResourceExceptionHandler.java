ae4edfead658b96a74ca2b961d60dcdc1e71ab20
GustaVoJorgeSilva <gustavo.jorge@hotmail.com.br>
2023-05-18 12:40:43 -0300

    Exception handling - delete

--- src/main/java/com/udemy/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.udemy.course.resources.exceptions;
 
+import com.udemy.course.services.exceptions.DataBaseException;
 import com.udemy.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -20,5 +21,13 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+        String error = "Data base error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 
 }
--- src/main/java/com/udemy/course/services/UserService.java
+++ src/main/java/com/udemy/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.udemy.course.services;
 
 import com.udemy.course.entities.User;
 import com.udemy.course.repository.UserRepository;
+import com.udemy.course.services.exceptions.DataBaseException;
 import com.udemy.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,11 +32,18 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+           throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataBaseException(e.getMessage());
+        }
+
 
     }
 
-    public User update(Long id, User obj){
+    public User update(Long id, User obj) {
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
